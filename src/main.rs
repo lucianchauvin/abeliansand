ece6d4f0fd378190ceb5d867775816b4d3389e37
Lucian Chauvin <lucianchauvin@gmail.com>
2025-12-27 13:34:15 -0600

    maybe a little faster

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{BufWriter};
+use std::collections::VecDeque;
 
-const N: usize = 2usize.pow(11) + 1;
+const N: usize = 2usize.pow(12) + 1;
 const C: usize = 4;
-const S: usize = 2usize.pow(23);
+const S: usize = 2usize.pow(28);
 
 const COLORS: [[u8; 3]; 4] = [
     [  0,   0,   0],
@@ -32,15 +33,17 @@ fn img(grid: &[[usize; N]; N]) -> std::io::Result<()> {
 }
 
 fn main() -> std::io::Result<()> {
-    let mut grid = [[0; N]; N];
-    let mut stack: Vec<(usize, usize)> = Vec::new();
+    let mut grid = Box::new([[0; N]; N]);
+    let mut stack: VecDeque<(usize, usize)> = VecDeque::new();
     let mut in_stack = [[false; N]; N];
 
-    grid[N/2][N/2] = S;
-    stack.push((N/2, N/2));
-    in_stack[N/2][N/2] = true;
+    let m = N / 2;
 
-    while let Some((x, y)) = stack.pop() {
+    grid[m][m] = S;
+    stack.push_back((m, m));
+    in_stack[m][m] = true;
+
+    while let Some((x, y)) = stack.pop_front() {
         in_stack[x][y] = false;
         if grid[x][y] >= C {
             let to_add = grid[x][y] / C;
@@ -58,20 +61,41 @@ fn main() -> std::io::Result<()> {
                 let ny = (y as isize + dy) as usize;
 
                 grid[nx][ny] += to_add;
-                if ny == N/2 || ny == nx {
-                    if (nx == N/2 && ny == nx) || (x != N/2 && y != N/2) {
+                if ny == m || ny == nx {
+                    if (nx == m && ny == nx) || (x != m && y != m) {
                         grid[nx][ny] += to_add;
                     }
                 }
                 
-                if grid[nx][ny] >= C && ny >= N/2 && ny <= nx && !in_stack[nx][ny] {
-                    stack.push((nx, ny));
+                if grid[nx][ny] >= C && ny >= m && ny <= nx && !in_stack[nx][ny] {
+                    stack.push_back((nx, ny));
                     in_stack[nx][ny] = true;
                 }
             }
         }
     }
 
-    let _ = img(&grid);
-    Ok(())
+
+    for x in m..N {
+        for y in m..=x {
+            let val = grid[x][y];
+            if val == 0 { continue; }
+
+            let dx = x - m;
+            let dy = y - m;
+
+            let points = [
+                (m + dx, m + dy), (m + dx, m - dy),
+                (m - dx, m + dy), (m - dx, m - dy),
+                (m + dy, m + dx), (m + dy, m - dx),
+                (m - dy, m + dx), (m - dy, m - dx),
+            ];
+
+            for (px, py) in points {
+                grid[px][py] = val;
+            }
+        }
+    }
+
+    img(&grid)
 }
